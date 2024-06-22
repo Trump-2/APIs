6e1b1fb850b43a5a75474d78ca513001d9e50d2c
Trump-2 <akashidaiki2@gmail.com>
2024-06-22 17:55:29 +0800

    使用 stripe API 的 SDK 來創造新的 customer

--- index19.php
+++ index19.php
@@ -4,10 +4,24 @@ require "API_keys.php";
 
 
 $data = [
-  'name' => 'Alice',
-  'email' => 'alice@example.com'
+  'name' => 'Brad',
+  'email' => 'brad@example.com'
 ];
 
+// 透過 SDK 的方式來創造 customer
+require __DIR__ . "/vendor/autoload.php";
+
+// 創造 StripeClient 物件，並傳入 API key
+$stripe = new \Stripe\StripeClient($for_stripe);
+
+// 創造新的 customer，並回傳該 customer 物件
+$customer = $stripe->customers->create($data);
+
+echo $customer;
+
+
+// 透過 cURL 的方式來創造 customer
+/*
 $ch = curl_init();
 
 curl_setopt_array(
@@ -15,7 +29,7 @@ curl_setopt_array(
   [
     CURLOPT_URL => 'https://api.stripe.com/v1/customers',
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_USERPWD => $stripe,
+    CURLOPT_USERPWD => $for_stripe,
     CURLOPT_POSTFIELDS => http_build_query($data)
   ]
 );
@@ -25,3 +39,4 @@ $response = curl_exec($ch);
 curl_close($ch);
 
 echo $response;
+*/
