1ac5dedce9d364f221a965dedb75648adb317c56
Wu-YingHong <akashidaiki2@gmail.com>
2024-06-23 12:39:13 +0800

    設定 status code

--- api/index.php
+++ api/index.php
@@ -13,4 +13,16 @@ $id = $parts[3] ?? null;
 
 echo $resource, ', ', $id;
 
-echo $_SERVER['REQUEST_METHOD'];
+echo $_SERVER['REQUEST_METHOD'], "\n";
+
+// 如果不是我們想要請求的 URL，則回傳 404 status code
+if ($resource != "tasks") {
+  // 設定回傳 Http 版本、404 的 status code 和 reason phase；第一種方式
+  // header("HTTP/1.1 404 Not Found!");
+  // 和上面一樣
+  // header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found!");
+
+  // 第二種方式 ( 推薦 )
+  http_response_code(404);
+  exit;
+}
